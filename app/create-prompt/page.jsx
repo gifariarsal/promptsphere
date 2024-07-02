f7fb61f02d03236d9953901e4a2468d0c601d22f
gifariarsal <117580307+gifariarsal@users.noreply.github.com>
2024-07-02 13:57:57 +0800

    feat: add Prompt model, define API route for create-prompt, add createPromptHandler function
    - Add Prompt model for managing prompt data
    - Define API route for handling create-prompt requests
    - Implement createPromptHandler function to handle prompt creation

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,14 +1,38 @@
 'use client';
 
 import Form from '@/components/Form';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 const CreatePromptPage = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
   const [submitting, setIsSubmitting] = useState(false);
   const [post, setPost] = useState({ prompt: '', tag: '' });
 
   const createPromptHandler = async (e) => {
-    console.log('createPromptHandler');
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    try {
+      const response = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        router.push('/');
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
   };
 
   return (
